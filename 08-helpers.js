748ecd1833073c575fe6a9277bcf9131abffb4f1
BrunoMatosSilva <brunomatos2010@outlook.com>
2022-08-26 17:36:35 -0300

    conceitos

---
+++ 08-helpers.js
@@ -0,0 +1,9 @@
+const tech = 'Node.js';
+const array = ['Node.js', 'React', 'Typescript'];
+
+const includesString = tech.includes('.');
+const includesArray = array.includes('React');
+
+const startsWith = tech.startsWith('N');
+const endsWith = tech.endsWith('js')
+console.log({endsWith});
