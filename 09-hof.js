7e6aafa16f757dff811838dd3b839f5ae605d84f
BrunoMatosSilva <bmsdevfullstack@gmail.com>
2022-08-27 20:13:21 -0300

    conceitos

--- 09-hof.js
+++ 09-hof.js
@@ -11,5 +11,27 @@ const find = array.find((product) => {
 const findIndex = array.findIndex((product) => {
   return product.name === 'Magic Mouse';
 });
-console.log({findIndex});
-console.log('Produto', array[findIndex]);
+//console.log({findIndex});
+//console.log('Produto', array[findIndex]);
+
+const some = array.some((product) => product.price < 1000);
+//console.log({some})
+
+const every = array.every((product) => product.price > 500);
+//console.log({every});
+
+const map = array.map((product) => {
+  return{
+    ...product,
+    subtotal: product.price * product.quantity,
+  }
+})
+//console.log({map});
+
+const filter = array.filter((product) =>  product.quantity > 1);
+//console.log({filter});
+
+const reduce = array.reduce((acc, product) => {
+  return acc + (product.price * product.quantity);
+}, 0);
+console.log({reduce});
